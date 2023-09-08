9000495103a47c1e0c0e888254e019b55abba63e
elizabethannan <elizabethyarteleyannan@gmail.com>
2023-09-08 21:14:28 +0000

    commit .html .js

---
+++ .js
@@ -0,0 +1,53 @@
+function getComputerChoice() {
+    const choices = ['Rock', 'Paper', 'Scissors'];
+    const randomIndex = Math.floor(Math.random() * 3);
+    return choices[randomIndex];
+  }
+  
+  function playRound(playerSelection, computerSelection) {
+    // Make playerSelection case-insensitive
+    playerSelection = playerSelection.toLowerCase();
+  
+    if (playerSelection === computerSelection.toLowerCase()) {
+      return "It's a tie!";
+    } else if (
+      (playerSelection === 'rock' && computerSelection === 'scissors') ||
+      (playerSelection === 'scissors' && computerSelection === 'paper') ||
+      (playerSelection === 'paper' && computerSelection === 'rock')
+    ) {
+      return `You Win! ${playerSelection} beats ${computerSelection}`;
+    } else {
+      return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    }
+  }
+  
+  function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+  
+    for (let i = 0; i < 5; i++) {
+      const playerSelection = prompt('Enter your choice (Rock, Paper, or Scissors):');
+      const computerSelection = getComputerChoice();
+  
+      const roundResult = playRound(playerSelection, computerSelection);
+      console.log(roundResult);
+  
+      if (roundResult.includes('Win')) {
+        playerScore++;
+      } else if (roundResult.includes('Lose')) {
+        computerScore++;
+      }
+    }
+  
+    if (playerScore > computerScore) {
+      console.log(`You win the game! Player: ${playerScore} - Computer: ${computerScore}`);
+    } else if (computerScore > playerScore) {
+      console.log(`You lose the game. Player: ${playerScore} - Computer: ${computerScore}`);
+    } else {
+      console.log(`It's a tie. Player: ${playerScore} - Computer: ${computerScore}`);
+    }
+  }
+  
+  // Start the game
+  game();
+  
